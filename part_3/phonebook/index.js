f6a4d5ca65b7c2502830f47ab1a18fa898de4c79
vihahuynh <huynhviha1703@gmail.com>
2021-09-30 21:03:42 +0700

    Complete 3.7 --> 3.8

--- part_3/phonebook/index.js
+++ part_3/phonebook/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const morgan = require("morgan");
+morgan.token("person", (req, res) => {
+  if (req.method === "POST") return JSON.stringify(req.body);
+  return null;
+});
 const app = express();
 app.use(express.json());
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :person"
+  )
+);
 
 let persons = [
   {
