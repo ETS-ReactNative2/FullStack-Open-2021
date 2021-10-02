501fa4c074dc282083de6bd8e83584903ad83ba6
vihahuynh <huynhviha1703@gmail.com>
2021-10-02 18:51:13 +0700

    Complete 4.3 --> 4.7

--- part_4/bloglist/tests/list_helper.test.js
+++ part_4/bloglist/tests/list_helper.test.js
@@ -1,16 +1,5 @@
 const listHelper = require("../utils/list_helper");
 
-const listWithOneBlog = [
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0,
-  },
-];
-
 const blogs = [
   {
     _id: "5a422a851b54a676234d17f7",
@@ -58,6 +47,17 @@ const blogs = [
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
     likes: 2,
+    __v: 0,
+  },
+];
+
+const listWithOneBlog = [
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
     __v: 0,
   },
 ];
