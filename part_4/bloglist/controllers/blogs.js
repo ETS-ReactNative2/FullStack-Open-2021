175c2f47874bfdce6ad71714675105c0cb7faeea
vihahuynh <huynhviha1703@gmail.com>
2021-10-04 20:47:35 +0700

    Complete 4.17

--- part_4/bloglist/controllers/blogs.js
+++ part_4/bloglist/controllers/blogs.js
@@ -1,28 +1,37 @@
 const BlogRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 BlogRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user");
   response.json(blogs);
 });
 
 BlogRouter.get("/:id", async (request, response) => {
   const id = request.params.id;
-  const blog = await Blog.findById(id);
+  const blog = await Blog.findById(id).populate("user");
   if (blog) response.json(blog);
   else response.status(404).end();
 });
 
 BlogRouter.post("/", async (request, response) => {
-  if (!request.body.url)
-    return response.status(400).json({ error: "URL is missing" });
+  const { title, url, author, likes, userId } = request.body;
+  if (!url) return response.status(400).json({ error: "URL is missing" });
 
-  if (!request.body.title)
-    return response.status(400).json({ error: "Title is missing" });
+  if (!title) return response.status(400).json({ error: "Title is missing" });
 
-  const blog = new Blog(request.body);
+  const user = await User.findById(userId);
+  const blog = new Blog({
+    url,
+    title,
+    author: author || null,
+    likes: likes || 0,
+    user: user._id,
+  });
 
   const addedBlog = await blog.save();
+  user.blogs = user.blogs.concat(addedBlog._id);
+  await user.save();
   response.status(201).json(addedBlog);
 });
 
@@ -35,7 +44,7 @@ BlogRouter.delete("/:id", async (request, response) => {
 BlogRouter.put("/:id", async (request, response) => {
   const id = request.params.id;
   const newBlog = {
-    likes: request.body.likes,
+    likes: {}.likes,
   };
 
   const updatedBlog = await Blog.findByIdAndUpdate(id, newBlog, { new: true });
