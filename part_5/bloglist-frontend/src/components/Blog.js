6e93c37c41b3e82e2a1c29fbf0abdb1da58c0bf2
vihahuynh <huynhviha1703@gmail.com>
2021-10-06 17:28:38 +0700

    Complete 5.7

--- part_5/bloglist-frontend/src/components/Blog.js
+++ part_5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
 
-export default Blog
+  const [showDetail, setShowDetail] = useState(false);
+  const handleShowDetail = () => setShowDetail(!showDetail);
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      <button onClick={handleShowDetail}>{showDetail ? "hide" : "view"}</button>
+      {showDetail && (
+        <>
+          <div>{blog.url}</div>
+          <div>
+            likes: {blog.likes}
+            <button>like</button>
+          </div>
+          <div>{blog.author}</div>
+        </>
+      )}
+    </div>
+  );
+};
+export default Blog;
