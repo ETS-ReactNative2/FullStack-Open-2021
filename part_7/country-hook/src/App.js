85597c3ddb1ee57a1a18e90f6064fbf5d56cfd8d
vihahuynh <huynhviha1703@gmail.com>
2021-10-14 16:20:55 +0700

    Complete 7.7

---
+++ part_7/country-hook/src/App.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null);
+  const url = `https://restcountries.com/v3.1/name/${name}?fullText=true`;
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        const foundCountry = response.data[0];
+        foundCountry.found = 1;
+        setCountry(foundCountry);
+      })
+      .catch(() => console.log("Country not found"));
+  }, [name, url]);
+
+  return country;
+};
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null;
+  }
+
+  if (!country.found) {
+    return <div>not found...</div>;
+  }
+
+  return (
+    <div>
+      <h3>{country.name.common} </h3>
+      <div>capital {country.capital[0]} </div>
+      <img
+        src={country.flags.png}
+        height="100"
+        alt={`flag of ${country.name.common}`}
+      />
+    </div>
+  );
+};
+
+const App = () => {
+  const nameInput = useField("text");
+  const [name, setName] = useState("");
+  const country = useCountry(name);
+
+  const fetch = (e) => {
+    e.preventDefault();
+    setName(nameInput.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  );
+};
+
+export default App;
