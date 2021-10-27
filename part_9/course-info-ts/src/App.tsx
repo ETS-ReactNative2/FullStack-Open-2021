0384a85c31d933bc69c845971b6196940bce880b
vihahuynh <huynhviha1703@gmail.com>
2021-10-27 12:16:33 +0700

    Complete 9.14

---
+++ part_9/course-info-ts/src/App.tsx
@@ -0,0 +1,64 @@
+const Header = ({ name }: { name: string }) => {
+  return <h1>{name}</h1>;
+};
+
+interface ContentProps {
+  name: string;
+  exerciseCount: number;
+}
+
+interface Courses {
+  courses: ContentProps[];
+}
+
+const Content = ({ courses }: Courses): JSX.Element => {
+  return (
+    <div>
+      {courses.map((course: ContentProps) => (
+        <p key={course.name}>
+          {course.name} {course.exerciseCount}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ courses }: Courses): JSX.Element => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {courses.reduce(
+        (carry: number, part: ContentProps) => carry + part.exerciseCount,
+        0
+      )}
+    </p>
+  );
+};
+
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7,
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14,
+    },
+  ];
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content courses={courseParts} />
+      <Total courses={courseParts} />
+    </div>
+  );
+};
+
+export default App;
