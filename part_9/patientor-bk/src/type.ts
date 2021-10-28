63fa70739f1a3aa278727ccb1e99a7a415167132
vihahuynh <huynhviha1703@gmail.com>
2021-10-28 15:41:03 +0700

    Complete 9.19

--- part_9/patientor-bk/src/type.ts
+++ part_9/patientor-bk/src/type.ts
@@ -38,7 +38,7 @@ interface SickLeave {
 interface OccupationalHealthcareEntry extends BaseEntry {
   type: "OccupationalHealthcare";
   employerName: string;
-  sickLeave: SickLeave;
+  sickLeave?: SickLeave;
 }
 
 interface Discharge {
